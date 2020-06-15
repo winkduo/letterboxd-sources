7b2996bcdd206bdde6b8bedb88997ec9d5566c91
Yigit Ozkavci <yigitozkavci8@gmail.com>
2020-06-15 23:36:01 +0100

    Set up bootstrap and mustache.js.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const purescript = require("./output/Main")
+const purescript = require ("./output/Main")
+const Mustache = require ("mustache")
 
-purescript.main()
+var mainTemplate = document.getElementById("main").innerHTML
+var renderedMain = Mustache.render(mainTemplate, { services: [{service: {  id: 1, name: "S1" }}] })
+
+document.getElementById("container").innerHTML = renderedMain
+
+purescript.main ()
--- src/Main.purs
+++ src/Main.purs
@@ -6,13 +6,12 @@ import Control.Monad.Except (runExceptT)
 import Data.Either
 import Effect (Effect)
 import Effect.Aff (launchAff_)
-import Effect.Class.Console (error)
+import Effect.Class.Console (error, log)
 import Letterboxd.Html (addServices)
 
 main :: Effect Unit
-main =
-  launchAff_ $ do
-    result <- runExceptT addServices
-    case result of
-      Left err -> error $ show err
-      Right _ -> pure unit
+main = launchAff_ $ do log "Started the extension!"
+    {-- result <- runExceptT addServices --}
+    {-- case result of --}
+    {--   Left err -> error $ show err --}
+    {--   Right _ -> pure unit --}
